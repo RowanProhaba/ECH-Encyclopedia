0da7999aca986db838744f70b3f97f6ecd9bf7ee
Rowan Prohaba <rowanchang@prohaba.com>
2026-06-09 15:03:20 +0800

    first commit

---
+++ includes/class-ech-encyclopedia-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://127.0.0.1
+ * @since      1.0.0
+ *
+ * @package    Ech_Encyclopedia
+ * @subpackage Ech_Encyclopedia/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Ech_Encyclopedia
+ * @subpackage Ech_Encyclopedia/includes
+ * @author     Rowan Chang <rowanchang@prohaba.com>
+ */
+class Ech_Encyclopedia_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-ech-encyclopedia-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://127.0.0.1
+ * @since      1.0.0
+ *
+ * @package    Ech_Encyclopedia
+ * @subpackage Ech_Encyclopedia/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Ech_Encyclopedia
+ * @subpackage Ech_Encyclopedia/includes
+ * @author     Rowan Chang <rowanchang@prohaba.com>
+ */
+class Ech_Encyclopedia_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
